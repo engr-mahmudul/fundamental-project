f4f96864420f043e0f2afb799f7c3141438fda10
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-01-02 17:24:30 +0200

    Js Shorthand for if/else

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 const Header = () => {
+    let marks = 85;
     return (
         <div>
             <ul>
                 <li>Home</li>
                 <li>About</li>
             </ul>
+            {
+                marks>80?
+                <h1>A++</h1>
+                :
+                <h1>Pass</h1>
+            }
         </div>
     );
 };
