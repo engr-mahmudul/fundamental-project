0c6d1b6a413b80b409d4615fe950a9e58d0ae1a4
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-01-05 23:42:04 +0200

    Basic Tailwind works perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 const App = () => {
  
   return (
@@ -7,6 +5,15 @@ const App = () => {
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <div>
+      <p className="text-xl text-red-500 bg-green-500">Tom</p>
+      <p className="text-xs text-white-600 text-center bg-yellow-300 bg-opacity-70">Jerry</p>
+      <p className="text-5xl text-white text-right bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 underline">Max</p>
+    </div>
+    <div className="bg-[url('https://www.shutterstock.com/image-vector/web-development-coding-cross-platform-260nw-1128653108.jpg')] h-lvh bg-no-repeat">
+
+    </div>
+
     </div>
   );
 };
